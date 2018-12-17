486b8b5ced76d466c26cf05bee9945ffc7890413
SonyaMirnaya <45785581+SonyaMirnaya@users.noreply.github.com>
2018-12-17 10:22:21 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,17 @@
+/**
+ * main.c -- программа "Hello, students"
+ *
+ *Copyright (c) 2016, Мирная Софья <mirnaya@cs.karelia.ru>
+ *
+ *This code is licensed under a MIT-style lilense
+ */
+
+#include <stdio.h>
+int main()
+{
+    /* Выводим приветствие */
+    fprintf(stdout, "Hello, students!\n");
+ 
+    return  0;
+}
+
